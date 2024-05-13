a1f72dd9da2241947fd7f86fc4f2da74f16bb5cd
chandantiwari <chandan13tiwaris@gmail.com>
2024-05-13 13:32:14 +0530

    liquibase changes

---
+++ src/main/java/com/poc/liquibasedemo/entity/Student.java
@@ -0,0 +1,31 @@
+package com.poc.liquibasedemo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "student")
+public class Student {
+    @Id
+    @Column(name = "stuId")
+    private int id;
+
+    @Column(name = "stuName")
+    private String name;
+
+    @Column(name = "stuSchool")
+    private String school;
+
+    @Column(name = "stuEmail")
+    private String email;
+}
---
+++ src/main/resources/db/changelog/changelog.sql
@@ -0,0 +1,17 @@
+-- liquibase formatted sql
+
+-- changeset chandantiwari:1715584481149-1
+-- preconditions onFail:MARK_RAN onError:MARK_RAN
+-- precondition-sql-check expectedResult:0 select count(*) from information_schema.tables where table_name='employee'
+CREATE TABLE "employee" ("emp_id" INTEGER NOT NULL, "emp_company" VARCHAR(255), "emp_email" VARCHAR(255), "emp_name" VARCHAR(255), CONSTRAINT "employee_pkey" PRIMARY KEY ("emp_id"));
+-- rollback DROP TABLE employee
+
+-- changeset chandantiwari:1715584526242-1
+-- precondition-sql-check expectedResult:0 select count(*) from employee
+INSERT INTO "employee" ("emp_id", "emp_company", "emp_email", "emp_name") VALUES (1, 'Google', 'chandan.tiwari@google.com', 'Chandan Tiwari');
+INSERT INTO "employee" ("emp_id", "emp_company", "emp_email", "emp_name") VALUES (2, 'Accenture', 'rajat.kumar@Accenture.com', 'Rajat Kumar');
+-- rollback TRUNCATE TABLE employee
+
+-- changeset chandantiwari:23123134
+CREATE TABLE "student" ("stu_id" INTEGER NOT NULL, "stu_school" VARCHAR(255), "stu_email" VARCHAR(255), "stu_name" VARCHAR(255), CONSTRAINT "student_pkey" PRIMARY KEY ("stu_id"));
+-- rollback DROP TABLE student
